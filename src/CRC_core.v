00212c27a731327b8a56885c430090eca7930ef2
AdaMahdavi <ayuyu.wrd@gmail.com>
2026-03-07 16:12:58 -1000

    Added input signal comments in CRC_core.v
    Updated comments for input signals in CRC_core module.

--- src/CRC_core.v
+++ src/CRC_core.v
@@ -3,14 +3,18 @@
 
 module CRC_core (
 
-input wire i_clk,
-input wire i_rstn,
-input wire [63:0] i_8xframe,
-input wire i_valid,
-input wire i_last,
-input wire i_corrupt,
+    input wire i_clk,
+    input wire i_rstn,
+    input wire [63:0] i_8xframe,    //frame data arriving in 8-byte bus per clock cycle
+  
+    input wire i_valid,             //marks the start of transition and MUST remain high until the very last 8-bytes of your frame are received. 
+                                    //If it's de-asserted before i_last is asserted, frame is invalid and both CRC and all internal registers must be reset.
+  
+    input wire i_last,              //marks the end of payload + FCS
+  
+    input wire i_corrupt,           //marks corrupted frame and stops processing transaction, remains high as long as the i_8xframe data on wire belongs to a corrupted frame
 
-output reg [31:0] o_crc
+    output reg [31:0] o_crc         //32-bit CRC output updated per new 8-byte input until i_last is asserted
 );
 
 
